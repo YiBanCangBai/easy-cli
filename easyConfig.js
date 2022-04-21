b40a35c0aaee5326cf16c9b0b06aa6892e02af26
247111532@qq.com <247111532@qq.com>
2022-04-21 11:26:51 +0800

    easy config

--- easyConfig.js
+++ easyConfig.js
@@ -1,8 +1,14 @@
 module.exports = {
-  'vue-single': {
-    name: '',
-    repo: '',
-    downloadUrl: '',
-    description: '',
+  'vue': {
+    name: 'easy-template-vue',
+    repo: 'https://codeup.aliyun.com/tssl/tssl/frontend_data_features/foundation/templates/easy-template-vue',
+    downloadUrl: 'git@codeup.aliyun.com:tssl/tssl/frontend_data_features/foundation/templates/easy-template-vue.git',
+    description: '基于vue3.0的单页项目模版',
+  },
+  'react': {
+    name: 'easy-template-react',
+    repo: 'https://codeup.aliyun.com/tssl/tssl/frontend_data_features/foundation/templates/easy-template-vue',
+    downloadUrl: 'git@codeup.aliyun.com:tssl/tssl/frontend_data_features/foundation/templates/easy-template-vue.git',
+    description: '测试react',
   },
 }
