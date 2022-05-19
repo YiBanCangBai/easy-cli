9ecec7211b99986e2684598a2cf9955d0c8a4917
247111532@qq.com <247111532@qq.com>
2022-05-19 16:09:12 +0800

    fix

--- easyConfig.js
+++ easyConfig.js
@@ -1,12 +1,12 @@
 module.exports = {
   'vue': {
-    name: 'easy-template-vue',
+    name: 'easy-temporary-vue',
     repo: 'https://codeup.aliyun.com/tssl/tssl/frontend_data_features/foundation/templates/easy-template-vue',
     downloadUrl: 'git@codeup.aliyun.com:tssl/tssl/frontend_data_features/foundation/templates/easy-template-vue.git',
     description: 'Based on vue-cli5.0 use vue3.0',
   },
   'react': {
-    name: 'easy-template-react',
+    name: 'easy-temporary-react',
     repo: 'https://codeup.aliyun.com/tssl/tssl/frontend_data_features/foundation/templates/easy-template-vue',
     downloadUrl: 'git@codeup.aliyun.com:tssl/tssl/frontend_data_features/foundation/templates/easy-template-vue.git',
     description: 'test react',
