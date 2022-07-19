c1c9287adf591e6e101a112840ddd728d8a1a898
247111532@qq.com <247111532@qq.com>
2022-07-19 15:34:13 +0800

    addd vue2 template

--- easyConfig.js
+++ easyConfig.js
@@ -1,3 +1,10 @@
+/*
+ * @Author       : Evan Chan
+ * @Date         : 2022-04-21 16:52:05
+ * @LastEditors  : evan
+ * @LastEditTime : 2022-07-19 15:33:18
+ * @Description  : 请填写简介
+ */
 module.exports = {
   'vue': {
     name: 'easy-template-vue',
@@ -5,10 +12,16 @@ module.exports = {
     downloadUrl: 'git@codeup.aliyun.com:tssl/tssl/frontend_data_features/foundation/templates/easy-template-vue.git',
     description: 'Based on vue-cli5.0 use vue3.0',
   },
-  'react': {
-    name: 'easy-template-react',
+  'vue3': {
+    name: 'easy-template-vue',
     repo: 'https://codeup.aliyun.com/tssl/tssl/frontend_data_features/foundation/templates/easy-template-vue',
     downloadUrl: 'git@codeup.aliyun.com:tssl/tssl/frontend_data_features/foundation/templates/easy-template-vue.git',
-    description: 'test react',
+    description: 'Based on vue-cli5.0 use vue3.0',
+  },
+  'vue2': {
+    name: 'easy-template-vue2',
+    repo: 'https://codeup.aliyun.com/tssl/tssl/frontend_data_features/foundation/templates/easy-template-vue2',
+    downloadUrl: 'git@codeup.aliyun.com:tssl/tssl/frontend_data_features/foundation/templates/easy-template-vue2.git',
+    description: 'Based on @vue/cli 5.0.1 use vue2.x',
   },
 }
